5240785e0293690f72a557dc155755f8f00390d8
Dezso Modos <dezso.modos@netbiol.elte.hu>
2016-01-22 11:21:36 +0000

    try

--- .idea/Main.py
+++ .idea/Main.py
@@ -14,5 +14,48 @@ Output:
 The script will give you a particular matrix of similarity between two graph, where the nodes are related.
 """
 
+def giancomponenet(G):
+    '''
+    Simple function. Makes form a graph a giant componenet graph.
+    '''
+    print "Making giant componenet"
+    import igraph as IG
+    components = G.clusters(IG.WEAK)
+    gr2="giantcomponent.txt"
+    IG.save(components.giant(), gr2, format="ncol")
+    G = IG.Graph.Read_Ncol(open(gr2,"rb"),names=True, weights="if_present", directed=True)
+    return G
+
+
+def CRfilereader(grafunk,inp):
+    '''
+    Reads cancer related data file.
+    '''
+    cancerrelated=[]
+    inp=open(inp,"rb")
+    FN=0
+    grafunk.vs["CRtype"]=0
+    for line in inp:
+        line=line.strip()
+        line=line.split("\t")
+        try:
+            int (line[1])
+            if int(line[1])==1:
+                cancerrelated.append(line[0])
+        except:
+            continue
+
+    for a in grafunk.vs:
+
+        if a['name'] in cancerrelated:
+            a["CRtype"]=1
+            szomszed=a.neighbors()
+            for elem in szomszed:
+                if elem["CRtype"]!=1:
+                    elem["CRtype"]=2
+                    FN=FN+1
+    print "First neighbors: ", FN
+    print "cancer related: ",len(cancerrelated)
+    return grafunk
 
 
