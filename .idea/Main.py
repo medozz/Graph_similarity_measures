6a57d6c2155fa81ec58ffb8f4aa7fdc1b7c30d58
Dezso Modos <dezso.modos@netbiol.elte.hu>
2016-01-22 13:46:39 +0000

    MAin_readyto_run

--- .idea/Main.py
+++ .idea/Main.py
@@ -20,28 +20,21 @@ def import_nodes(file_name,separator,default_weight, header):
     :param separator: separator of the import file
     :param default_weight: 0 if there is no default wheight and use the files wheights
     :param header: if true there is a header, if false there is no header
-    :return: id_wieght list of tuples with string id and float weight pairs
+    :return: id_wieght dictionarry with string key and float weights
     """
-    id_weight = []
+    id_weight = {}
     inp = open(file_name)
     if header == True:
         header = inp.readline()
     for line in inp:
         line = line.split(sep)
         if len(line) > 1 and default_weight==0:
-            id_weight.append((str(line[0]),float(line[1])))
+            id_weight[str(line[0])] = float(line[1])
         elif float(default_weight) > 0:
-            id_weight.append((str(line[0]),float(line[1])))
+            id_weight[str(line[0])] = float(default_weight)
         else:
-            id_weight.append((str(line[0]),1.0))
+            id_weight[str(line[0])] = 1.0
     return id_weight
-
-
-
-
-
-
-
 
 
 def giancomponenet(G):
@@ -49,43 +42,69 @@ def giancomponenet(G):
     Simple function. Makes form a graph a giant componenet graph.
     '''
     print "Making giant componenet"
-    import igraph as IG
     components = G.clusters(IG.WEAK)
     gr2="giantcomponent.txt"
-    IG.save(components.giant(), gr2, format="ncol")
-    G = IG.Graph.Read_Ncol(open(gr2,"rb"),names=True, weights="if_present", directed=True)
+    igraph.save(components.giant(), gr2, format="ncol")
+    G = igraph.Graph.Read_Ncol(open(gr2,"rb"),names=True, weights="if_present", directed=True)
     return G
 
 
-def CRfilereader(grafunk,inp):
-    '''
-    Reads cancer related data file.
-    '''
-    cancerrelated=[]
-    inp=open(inp,"rb")
+def neighbors_flow_propagation(vertexes, information, used_vertexes, outgraf):
+    """
+
+    :param vertexes: The vertxes, whose neighbors are searched for
+    :param information: The amount of informatioon which cvommes in
+    :param used_vertexes: the set of used vertexies
+    :return: added used vertexes, nighbor vertexes, for the next step
+    """
+    for vertex in vertexes:
+        neighbors_of_vertex=vertex.neighbors()
+        for one in neighbors_of_vertex:
+            if one not in used_vertexes:
+                one["Reach"]= one["Reach"]+information*0.5
+    for one in vertexes.neighbors():
+        used_vertexes.add(one)
+    neighbor_vertexes = vertexes.neighbors()
+    return used_vertexes, neighbor_vertexes, outgraf
+
+
+def relatedness_count(ourgraf,id_weight,neighborhood_number):
+    """
+    :param ourgraf: igraph graph file
+    :param id_weight: id_weights from the funcition import_nodes
+    :param neighborhood_number int, tells how far4 should igraph chech the neighbors
+    :return: igraph graph object with id_weight in
+
+    Weigt propagation is not changable jet. Curently used the function as 2^-n*(income weight)
+    Later can be used the edge weight of the particular graph, or thinking about different function.
+    """
+    related=[]
     FN=0
-    grafunk.vs["CRtype"]=0
-    for line in inp:
-        line=line.strip()
-        line=line.split("\t")
-        try:
-            int (line[1])
-            if int(line[1])==1:
-                cancerrelated.append(line[0])
-        except:
-            continue
-
-    for a in grafunk.vs:
-
-        if a['name'] in cancerrelated:
-            a["CRtype"]=1
-            szomszed=a.neighbors()
-            for elem in szomszed:
-                if elem["CRtype"]!=1:
-                    elem["CRtype"]=2
-                    FN=FN+1
-    print "First neighbors: ", FN
-    print "cancer related: ",len(cancerrelated)
-    return grafunk
+    ourgraf.vs["Reach"]=0
+    for vertex in ourgraf.vs:
+        if vertex['name'] in id_weight:
+            vertex["Reach"] = vertex["Reach"]+id_weight[vertex['name']]
+            used_vertexes=set(vertex)
+            neighbor_vertexes=set(vertex)
+            k=1
+            while k<neighborhood_number+1:
+                infromation = id_weight[vertex['name']]*2^-k
+                used_vertexes, neighbor_vertexes, outgraf=neighbors_flow_propagation(neighbor_vertexes, infromation,
+                                                                            used_vertexes, outgraf)
+                k=k+1
+    return ourgraf
 
 
+def outwirte(outgraf, file_name, sep):
+    out = open(file_name,"wb")
+    out.write("UPID"+sep+"Reach"+"\n")
+    for vertex in outgraf.vs:
+        out.write(vertex['name']+"\t"+str(vertex["Reach"])+"\n")
+    out.close()
+
+#Running commands
+id_weights = import_nodes(sample_nodes,"\t",1)
+G = igraph.Graph.Read_Ncol(open("sample.csv","rb"),names=True, weights="if_present", directed=True)
+G = giancomponenet(G)
+G = relatedness_count(G, id_weights, 1)
+outwirte(G, "sample_out.txt", "\t")
